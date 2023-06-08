6c60d41fe80aa73dcfbaccb4a23b2bfdd06de469
Jeff Sember <jpsember@gmail.com>
2023-06-08 13:20:05 -0700

    Issue #1

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module golang-diskutils
 
 go 1.20
 
-require github.com/jpsember/golang-base v0.0.0-20230608190806-083c9d22b2e0
+require github.com/jpsember/golang-base v0.0.0-20230608200550-c72133056135
--- snapshot/snapshot_oper.go
+++ snapshot/snapshot_oper.go
@@ -5,6 +5,9 @@ import (
 	. "github.com/jpsember/golang-base/base"
 	. "github.com/jpsember/golang-base/files"
 	. "golang-diskutils/gen"
+	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -38,7 +41,7 @@ func (oper *SnapshotOper) UserCommand() string {
 func (oper *SnapshotOper) Perform(app *App) {
 	oper.SetVerbose(app.Verbose())
 
-	oper.outputDir = NewPathM(oper.config.OutputDir())
+	oper.outputDir = NewPathM(oper.config.OutputDir()).GetAbsM()
 	if !oper.outputDir.IsDir() {
 		BadArg("output directory doesn't exist:", oper.outputDir)
 	}
@@ -71,14 +74,43 @@ func (oper *SnapshotOper) constructPathBuffer() {
 }
 
 func (oper *SnapshotOper) takeSnapshot() {
-	Todo("takeSnapshot")
+	timestamp := time.Now().UnixMilli()
+	for devNum := 0; devNum < int(oper.config.NumDevices()); devNum++ {
+		imagePath := oper.getNextImagePath(timestamp, devNum)
+		s := NewArray[string]()
+		dispArg := "-D" + IntToString(1+devNum)
+		s.Append(strings.Split("screencapture -S -T 1 -r -tjpg", " ")...)
+		s.Add(dispArg)
+		s.Add(imagePath.String())
+		result, err := makeSysCall(s.Array())
+		if strings.Contains(result, "Invalid display specified") {
+			Alert("Display "+dispArg, "failed:", INDENT, result)
+			continue
+		}
+		CheckOk(err, "system call failed:", err)
+		oper.imagePaths.Add(imagePath)
+	}
+}
+
+func makeSysCall(cmdLineArgs []string) (string, error) {
+	var commandName = cmdLineArgs[0]
+	var programArguments = cmdLineArgs[1:]
+	command := exec.Command(commandName, programArguments...)
+	out, err := command.CombinedOutput()
+	var strout = string(out)
+	return strout, err
 }
 
 func (oper *SnapshotOper) trimPathBuffer() {
 	Todo("have generated scalar int fields default to 'int', not 'int32'")
 	for oper.imagePaths.Size() > int(oper.config.MaxImages()) {
-		p := oper.imagePaths.First()
+		p := oper.outputDir.JoinPathM(oper.imagePaths.First())
 		oper.imagePaths.Remove(0, 1)
+		oper.Log("Deleting:", p)
 		p.DeleteFileM()
 	}
+}
+
+func (oper *SnapshotOper) getNextImagePath(timestamp int64, deviceNum int) Path {
+	return oper.outputDir.JoinM(IntToString(deviceNum) + "_" + strconv.FormatInt(timestamp, 10) + ".jpg")
 }
