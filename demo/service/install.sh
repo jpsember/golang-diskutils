8b6d8555cc132653ed407b6d2bb32cddaaef4bb3
Jeff Sember <jpsember@gmail.com>
2023-06-08 20:44:15 -0700

    Issue #2: I think I have it working

--- demo/service/install.sh
+++ demo/service/install.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
-plist_path="wtf.plist"
+plist_path="diskutils-snapshot.plist"
 plist_filename=$(basename "$plist_path")
-install_path="$HOME/Library/LaunchAgents"
+install_path="$HOME/Library/LaunchAgents/$plist_filename"
 
 echo "installing launchctl plist: $plist_path --> $install_path"
 
 cp -f "$plist_path" "$install_path"
-
-#sudo chown root "$install_path"
-#sudo chmod 644 "$install_path"
 
 echo "This may complain if it isn't already loaded..."
 launchctl unload "$install_path"
@@ -18,5 +15,5 @@ launchctl unload "$install_path"
 echo "...now attempting to load it:"
 launchctl load "$install_path"
 
-echo "to check if it's running, run this command:  launchctl list | grep wtf"
+echo "to check if it's running, run this command:  launchctl list | grep diskutils-snapshot"
 echo "to uninstall, run this command:  launchctl unload \"$install_path\""
