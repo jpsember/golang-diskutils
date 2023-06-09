51321ab8030bc8a6378479cfd3d08ed824eb6dfb
Jeff Sember <jpsember@gmail.com>
2023-06-08 20:06:05 -0700

    Issue #2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module golang-diskutils
 
 go 1.20
 
-require github.com/jpsember/golang-base v0.0.0-20230609021849-306d6b8b84cc
+require github.com/jpsember/golang-base v0.0.0-20230609024831-385041d9c86e
--- names/names_oper.go
+++ names/names_oper.go
@@ -103,11 +103,8 @@ func (oper Names) Perform(app *App) {
 	logPath := NewPathOrEmptyM(oper.config.Log())
 	if logPath.NonEmpty() {
 		logPath = app.StartDir().JoinM(logPath.String())
-		Todo("joined logPath to startDir:" + app.StartDir().String() + " result: " + logPath.String())
 	}
-	Todo("attempting to open logPath: " + logPath.String())
 	oper.errLog = NewErrLog(logPath)
-	Todo("it was opened")
 	oper.errLog.SkipHeader = app.HasTestArgs()
 	oper.errLog.Clean = oper.config.CleanLog()
 
@@ -118,8 +115,6 @@ func (oper Names) Perform(app *App) {
 	oper.printDiskUsage(rootInfo)
 	Pr(Dashes)
 	oper.errLog.PrintSummary()
-
-	Todo("and we are done performing")
 }
 
 func (oper Names) processDir(dirInfo DirInfo) {
